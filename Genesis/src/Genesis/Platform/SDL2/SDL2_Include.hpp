456942a42b80ae7cd7352884bc800bfba7bcd9d5
Joshua Masci <Iamshortman@gmail.com>
2019-03-05 21:36:24 -0800

    Updated to SDL2-2.0.9

--- Genesis/src/Genesis/Platform/SDL2/SDL2_Include.hpp
+++ Genesis/src/Genesis/Platform/SDL2/SDL2_Include.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-//Get rid of the SDL_main that messes up the build
-#define SDL_MAIN_HANDLED
-#include <SDL2/SDL_main.h>
+#include <SDL.h>
+#include <SDL_events.h>
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
-
-#include <SDL2\SDL_GameController.h>
-#include <SDL2\SDL_joystick.h>
+#include <SDL_GameController.h>
+#include <SDL_joystick.h>
